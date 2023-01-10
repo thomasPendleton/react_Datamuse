386175451d73fcef119be5fd4ecc00acc2f5cc84
Thomas Pendleton <thomas6576@yahoo.com>
2023-01-09 22:04:51 -0800

    reformat app and wordcontainer

--- datamuse_API/src/App.jsx
+++ datamuse_API/src/App.jsx
@@ -3,52 +3,56 @@ import "./App.css"
 import WordContainer from "./WordContainer"
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
   const [apiData, setApiData] = useState([])
   const [data, setData] = useState(null)
+
   const [input, setInput] = useState("")
 
   const fetchDatamuse = async (value) => {
+    setApiData([])
+    setLoading(true)
     try {
-      console.log(value)
       const response = await fetch(
         `https://api.datamuse.com/words?rel_rhy=${value}&max=20`
       )
       const data = await response.json()
-
       setApiData(data)
       console.log(data)
     } catch (error) {
       console.log(error)
     }
+    setLoading(false)
   }
 
   useEffect(() => {
     if (data === null) return
     fetchDatamuse(data)
   }, [data])
-
+  
   const handleSubmit = (e) => {
     e.preventDefault()
-
     setData(input)
   }
-
-  const handleChange = (e) => {
-    setInput(e.target.value)
-  }
   console.log(apiData)
+
   return (
     <div>
       <h1>Datamuse API</h1>
-     {/* add buttons for different api calls. */}
-      <form action="submit" onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" value={input} onChange={(e) => handleChange(e)} />
+      {/* add buttons for different api calls. */}
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="word">Your word </label>
+        <input
+          id="word"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
-      {
-        apiData.length > 0 ?  <WordContainer apiData={apiData} /> : null
-      }
-     
+      {loading ? <h3>Loading...</h3> : null}
+      {apiData.length > 0 ? <WordContainer apiData={apiData} /> : null}
     </div>
   )
 }
--- datamuse_API/src/WordContainer.jsx
+++ datamuse_API/src/WordContainer.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 
 const WordContainer = ({ apiData }) => {
-  console.log(apiData)
+  console.log(apiData);
   return (
     <div className="word-container">
       {apiData.map((word, idx) => {
-        return <h4 className="word" key={idx}>{word.word}</h4>
+        return (
+          <h4 className="word" key={idx}>
+            {word.word}
+          </h4>
+        )
       })}
     </div>
   )
